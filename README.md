9594e3934e0c02294f4e243acd4f52c11294f86a
natalyapeixoto <natalya_peixoto@hotmail.com>
2023-04-16 21:30:43 +0200

    make it work mobile

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 #### Drumkit 
-drum kit made as part of the final project for Reprograma
 
-### Technologies used: 
+cool drum kit
 
-* HTML
-* CSS
-* JavaScript
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 function playSound (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const code = e.keyCode || e.target.getAttribute('data-key')
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  const key = document.querySelector(`.key[data-key="${code}"]`);
   if(!audio) return;
-  console.log(audio)
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
 };
 
 function removeTransition(e){
+  console.log(Math.ceil(Math.random() * 10))
+  console.log(e.srcElement.innerHTML)
+  const oi = e.srcElement.innerHTML
   if (e.propertyName !== 'transform') return; 
   this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playSound))
 
 window.addEventListener('keydown', playSound);
-
-
-
